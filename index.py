545f775b5898524aaa3262dfe4940e80a267dd07
nooralshammasi <72754160+nooralshammasi@users.noreply.github.com>
2020-10-13 00:43:49 +0300

    Update index.py
    add new Powerades function

--- index.py
+++ index.py
@@ -5,8 +5,13 @@ import os
 print('hello worlds')
 
 def summ(x,y):
-	print('thanks')
+       print('thanks')
 
 
-	def mul(x,y):
-		print(x*y)
+def mul(x,y):
+       print(x*y)
+		
+		
+		
+def power(x,y):
+	print(x**y)
